929c0aff8f08a523b7ac0691c78a5abeff3c9b0e
Jelena Nikolic <nikolic_jelena@outlook.com>
2024-04-23 17:21:48 +0200

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'im-homework';
+  file: any;
+  selectedFiles?: FileList;
+  fileArray: any[] = [];
+  ifMinimum = false
+  upload = true
+  url:any
+  previews:any=[]
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+  }
+
+
+  uploadImage(event: any) {
+    this.ifMinimum = true
+    this.selectedFiles = event.target.files;
+    this.fileArray.push(event.target.files)
+
+    for (let i = 0; i < this.selectedFiles!.length; i++) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.previews.push(e.target.result);
+      };
+
+      reader.readAsDataURL(this.selectedFiles![i]);
+    }
+    
+  }
+
+  cancelAll(){
+    // this.selectedFiles = new FileList
+    this.previews = []
+    this.fileArray = []
+  }
+
+  validateImages() {
+    if (this.file.length === 5) {
+      this.upload = false
+    }
+  }
+
+  resize(){
+    //console.log(this.fileArray);
+    this.apiService.resizeAndUpload(this.fileArray).subscribe({
+      next: (data) => {
+        console.log('registered');
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    })
+    
+  }
+
+  download(){}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
